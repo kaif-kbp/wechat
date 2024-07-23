deb4cb42cfbee4dabadf5f38711c2b338b5b1442
kaif-kbp <kbp632003@gmail.com>
2024-07-23 16:32:53 +0530

    Update save.php

--- save.php
+++ save.php
@@ -1,20 +1,28 @@
 <?php
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $db_name = "wechat";
+
 $conn = new mysqli($servername, $username, $password , $db_name);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+    
+}else{
+    echo ('<script>alert("connection successful"); </script>');
 }
 
-$id = $_POST["Id"];
 $name = $_POST["Name"];
 $message= $_POST["Message"];
-$sql = "INSERT INTO login (Name , Phone) VALUES ('$cname' , '$contact')";
-if($conn-> query($sql) == true){
-    echo "inserted succesfully";
-}
 
+$sql = "INSERT INTO message ( Name , Message) VALUES ('$name' , '$message')";
+
+if($conn-> query($sql) == true){
+    echo '<script> console.log("inserted succesfully") </script>';
+}else{
+    echo ('<script>console.log($conn -> error); </script>');
+}
 
 ?>
